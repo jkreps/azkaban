3ef668a1383767a876e18ca36662150ab40df4ae
rpark <rpark@linkedin.com>
2010-03-25 17:10:08 -0700

    Adding Apache license info to Azkaban.

--- azkaban/src/java/azkaban/jobs/JavaJob.java
+++ azkaban/src/java/azkaban/jobs/JavaJob.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010 LinkedIn, Inc
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package azkaban.jobs;
 
 import java.lang.reflect.Method;
@@ -112,8 +128,8 @@ public class JavaJob extends AbstractJob {
 		}
 
 		try {
-			Utils.getMethod(_javaObject.getClass(), _runMethod, (Class<?>)null).invoke(
-					_javaObject, (Object)null);
+			Utils.getMethod(_javaObject.getClass(), _runMethod, new Class<?>[]{}).invoke(
+					_javaObject, new Object[]{});
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
