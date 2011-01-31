1aec3f6c9b47a1792175524a809b8d54195020a4
Eric Tschetter <cheddar@metamarketsgroup.com>
2011-01-31 11:02:22 -0800

    Got some merge text in Utils.java that shouldn't be there

--- azkaban-common/src/java/azkaban/common/utils/Utils.java
+++ azkaban-common/src/java/azkaban/common/utils/Utils.java
@@ -408,8 +408,6 @@ public class Utils {
         temp.deleteOnExit();
         return temp;
     }
-<<<<<<< HEAD
-=======
 
     /**
      * Read in content of a file and get the last *lineCount* lines. It is
@@ -551,5 +549,4 @@ public class Utils {
 
         return false;
     }
->>>>>>> b9f26a24f8745a89e701d7a42f922a6a4bc15888
 }
