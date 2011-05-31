2250e2bda9ccfded5e7b5b7a09051685c65bc25b
Ibrahim Ulukaya <iulukaya@adconion.com>
2011-05-31 19:41:14 -0400

    AZK-97: DoNothingJob returns null for generated props, causes NPE.

--- azkaban-common/src/unit/azkaban/common/jobs/DoNothingJobTest.java
+++ azkaban-common/src/unit/azkaban/common/jobs/DoNothingJobTest.java
@@ -1,46 +1,41 @@
 /*
- * Copyright 2010 LinkedIn, Inc
- * 
+ * Copyright 2011 Adconion, Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package azkaban.common.jobs;
 
-import azkaban.common.jobs.AbstractJob;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
 import azkaban.common.utils.Props;
 
 /**
- * A placeholder job that does nothing. Used for defining barriers for
- * convenient dependency management
- * 
- * E.g. this job can be used solely as a way of grouping dependencies
- * 
- * @author jkreps
- * 
+ * @author Ibrahim Ulukaya
+ *
  */
-public class DoNothingJob extends AbstractJob {
+public class DoNothingJobTest {
 
-    public DoNothingJob(String name, Props props) {
-        super(name);
+    @Test
+    public void testDoNothingJob() {
+        // Start a fictitious job.
+        String wfId = "23";
+        String job1Name = "Job1";
+        Props job1Props = new Props();
+        job1Props.put("azkaban.flow.id", wfId);
+        DoNothingJob job1 = new DoNothingJob(job1Name, job1Props);
+
+        Assert.assertNotNull(job1.getJobGeneratedProperties());
     }
-
-    public void run() {
-    // does nothing, just a placeholder for dependencies
-    }
-
-    @Override
-    public Props getJobGeneratedProperties() {
-        return new Props();
-    }
-
 }
